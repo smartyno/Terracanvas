1ff234d4a59859711272566904cbd694c5332ef3
smartyno <93627944+smartyno@users.noreply.github.com>
2023-10-09 22:31:03 +0200

    Update terracanvas.js

--- terracanvas.js
+++ terracanvas.js
@@ -1,13 +1,32 @@
 let svgList = [];
 let grid = [];
 const gridSize = 32;
-const svgWidth = 32;
-const svgHeight = 32;
+const svgWidth = 16;
+const svgHeight = 16;
 let selectedSVG = 1;
 let input;
 
+// Define three different color palettes (fill, stroke, background)
+const colorPalettes = [
+  {
+    fill: '#FF5733',    // Fill color (Palette 1)
+    stroke: '#0066FF',  // Stroke color (Palette 1)
+    background: '#FFFFCC' // Background color (Palette 1)
+  },
+  {
+    fill: '#00FF66',    // Fill color (Palette 2)
+    stroke: '#FF3333',  // Stroke color (Palette 2)
+    background: '#66CCFF' // Background color (Palette 2)
+  },
+  {
+    fill: '#FFCC00',    // Fill color (Palette 3)
+    stroke: '#6600FF',  // Stroke color (Palette 3)
+    background: '#CCFFFF' // Background color (Palette 3)
+  }
+];
+
 function preload() {
-  // Load 2 SVG files from the GitHub repository
+  // Load 5 SVG files from the GitHub repository
   for (let i = 0; i < 3; i++) {
     // Replace 'username/repo' and 'path/to/svg' with your GitHub repository's information
     let githubRawURL = `https://raw.githubusercontent.com/smartyno/Terraforms/main/symbols/s${i + 1}.svg`;
@@ -22,14 +41,14 @@ function setup() {
   for (let x = 0; x < gridSize; x++) {
     grid[x] = [];
     for (let y = 0; y < gridSize; y++) {
-      grid[x][y] = floor(random(1, 4)); // Assign random numerical IDs (1 or 2)
+      grid[x][y] = floor(random(1, 3)); // Assign random numerical IDs (1 to 5)
     }
   }
 
   // Create an input field for numeric input
   input = createInput();
   input.position(10, height + 10);
-  input.attribute('placeholder', 'Enter SVG ID (1-3)');
+  input.attribute('placeholder', 'Enter SVG ID (1-5)');
   input.input(handleInput);
 }
 
@@ -40,7 +59,18 @@ function draw() {
   for (let x = 0; x < gridSize; x++) {
     for (let y = 0; y < gridSize; y++) {
       let svgID = grid[x][y];
+
+      // Apply the selected color palette to the SVG
+      let paletteIndex = (svgID - 1) % colorPalettes.length;
+      let palette = colorPalettes[paletteIndex];
+
+      push();
+      fill(palette.fill);
+      stroke(palette.stroke);
+      strokeWeight(2);
+      rect(x * svgWidth, y * svgHeight, svgWidth, svgHeight);
       image(svgList[svgID - 1], x * svgWidth, y * svgHeight, svgWidth, svgHeight);
+      pop();
     }
   }
 }
@@ -64,6 +94,6 @@ function handleInput() {
     selectedSVG = int(newValue);
     console.log(`Selected SVG: ${selectedSVG}`);
   } else {
-    console.error("Invalid input. Enter a numeric value between 1 and 5.");
+    console.error("Invalid input. Enter a numeric value between 1 and 3.");
   }
 }
